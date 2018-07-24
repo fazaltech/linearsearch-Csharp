5f4f2f34a731fdaafef730e220e43d7ccfa6c403
Muhammd Fazal <fazal.abbas4@gmail.com>
2018-07-24 22:17:23 +0500

    Initial commit

---
+++ linearsearch Csharp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linearsearch_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            int  i, num, n, c = 0;
+            int pos = 0;
+
+            Console.WriteLine("Enter the array size : ");
+            n= int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+            Console.WriteLine("Enter Array Elements : ");
+            for (i = 0; i < n; i++)
+            {
+                arr[i]= int.Parse(Console.ReadLine());
+            }
+
+
+            Console.WriteLine("Enter the number to be search : ");
+            num= int.Parse(Console.ReadLine());
+            for (i = 0; i < n; i++)
+            {
+                if (arr[i] == num)
+                {
+                    c = 1;
+                    pos = i + 1;
+                    break;
+                }
+            }
+            if (c == 0)
+            {
+                Console.WriteLine("Number not found..!!");
+            }
+            else
+            {
+                
+                Console.WriteLine(num+" found at position "+pos);
+            }
+            Console.Read();
+
+        }
+    }
+}
